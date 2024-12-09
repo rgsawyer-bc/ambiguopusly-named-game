90716aecaacd12b360e3460c4b0ce4b175b15071
Graham <36865449+snaphap@users.noreply.github.com>
2024-12-08 19:01:31 -0500

    it works now
    look guys look its working

--- game.py
+++ game.py
@@ -1,63 +1,99 @@
 from tiles import *
+import time
 
 class Fart:
-    def __init__(self, coords, puzzle, direction = 1):
+    def __init__(self, coords, puzzle, direction = 1, start = 1):
         """direction is 1 or -1, 1 is for down"""
-        self.coords = coords
         self.direction = direction
         self.puzzle = puzzle
-        self.emoji = "w"
+        self.emoji = "💨"
+        self.start = start
         
         self.x, self.y, self.z = coords
+
+        self.board = puzzle.boards[self.z]
 
 
     def up(self):
         gravity = self.puzzle[self.z].gravity
-        self.x += gravity[0]
-        self.y += gravity[1]
+        x = self.x - gravity[0]
+        y = self.y - gravity[1]
+        return [x, y, self.z]
 
     
     def down(self):
-        self.coords[0] += 1
-
-
-    def left(self):
-        self.coords[1] -= 1
+        gravity = self.puzzle[self.z].gravity
+        x = self.x + gravity[0]
+        y = self.y + gravity[1]
+        return [x, y, self.z]
 
 
     def right(self):
-        self.coords[1] += 1
+        gravity = self.puzzle[self.z].gravity
+        if gravity[0] == 0:
+            x = self.x - gravity[1]
+            return [x, self.y, self.z]
+        else:
+            y = self.y + gravity[0]
+            return [self.x, y, self.z]
 
 
-    def move(self):
-        self.coords[0] += self.direction
+    def left(self):
+        gravity = self.puzzle[self.z].gravity
+        if gravity[0] == 0:
+            x = self.x + gravity[1]
+            return [x, self.y, self.z]
+        else:
+            y = self.y - gravity[0]
+            return [self.x, y, self.z]
 
 
-    def teleport(self, location):
-        self.coords = location
+    def moveto(self, location):
+        self.x, self.y, self.z = location
+
+
+    def coords(self):
+        return [self.x, self.y, self.z]
+    
+
+    def tile(self):
+        return self.puzzle[self]
 
 
     def __index__(self):
-        return self.coords[1], self.coords[0]
+        return tuple(self.coords())
+    
+
+    def __getitem__(self, index):
+        return [self.x, self.y, self.z][index]
+    
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.z})"
 
 
 class Board:
-    def __init__(self, width, height, tiles):
+    def __init__(self, width, height, tiles, gravity = "down"):
         self.width = width
         self.height = height
 
         if len(tiles) != width * height:
             raise ValueError("dumbass")
-        
+
+        self.tiles = tiles
+        self.setGravity(gravity) # oh boy this shits gonna get weird
+            # instead of making each thing refer explicitly to a relative index, make a function that returns the "left direction" (or other directions) given a gravity
+
+
+    def setPuzzle(self, puzzle):
+        self.puzzle = puzzle
+        width, height = self.width, self.height
         board = [[Air for x in range(width)] for y in range(height)]
         for x in range(width):
             for y in range(height):
-                board[y][x] = tiles[(width)*y + x](self)
+                board[y][x] = self.tiles[(width)*y + x](puzzle)
 
         self.tiles = board
-
-        self.gravity = [0, 1] # oh boy this shits gonna get weird
-            # instead of making each thing refer explicitly to a relative index, make a function that returns the "left direction" (or other directions) given a gravity
 
 
     def setGravity(self, gravity):
@@ -68,14 +104,21 @@ class Board:
             self.gravity = [0, -1]
         elif gravity == "right":
             self.gravity = [1, 0]
+        elif gravity == "left":
+            self.gravity = [-1, 0]
         else:
-            self.gravity == [-1, 0]
+            raise ValueError('BUNGLE')
 
 
     def __str__(self):
+        boardIndex = self.puzzle.boards.index(self)
+        tileEmojis = [[tile.emoji for tile in j] for j in self.tiles]
+        for fart in self.puzzle.farts:
+            if fart.z == boardIndex:
+                tileEmojis[fart.y][fart.x] = fart.emoji
         rawtiles = "\n".join([
-            " ".join([tile.emoji for tile in j])
-        for j in self.tiles])
+            " ".join(j)
+        for j in tileEmojis])
 
         return rawtiles
     
@@ -85,19 +128,41 @@ class Board:
             return self.tiles[index[1]][index[0]]
         else:
             return self.tiles[index]
+        
+
+    # define in boolean to check if fart in board
     
 
 class Puzzle:
-    def __init__(self, boards):
+    def __init__(self, boards, startTiles = [Start1]):
         self.boards = boards
-        self.fart = Fart([0, 0, 0], self)
+        for board in boards:
+            board.setPuzzle(self)
+        self.farts = []
+        self.startTiles = startTiles
+        self.state = "initializing"
 
 
     def __getitem__(self, index):
-        return self.boards[index]
+        if isinstance(index, Fart):
+            return self.boards[index[2]][index]
+        else:
+            return self.boards[index]
+        
+
+    def __setitem__(self, fart, tile):
+        self[fart.z][fart.y][fart.x] = tile
 
 
-    def findTiles(self, tile):
+    def __str__(self):
+        return "\n\n\n".join([str(board) for board in self.boards])
+
+
+    def createFart(self, coords):
+        self.farts.append(Fart(coords, self))
+
+
+    def findTileIndeces(self, tile):
         foundIndeces = []
         for z, board in enumerate(self.boards):
             for y in range(board.height):
@@ -105,14 +170,74 @@ class Puzzle:
                     currentTile = board[y][x]
                     if currentTile == tile:
                         foundIndeces.append([x, y, z])
+
+        return foundIndeces
+
+    
+    def update(self):
+        print(puzzle)
+        for fart in self.farts:
+            tile = fart.tile()
+            tile.activate(fart)
+            fart.moveto(tile.next(fart))
+        print("\n")
+
+
+    def start(self):
+        for startTile in self.startTiles:
+            startIndeces = self.findTileIndeces(startTile)
+            startIndex = int(input("Start at "))
+            self.createFart(startIndeces[startIndex])
+
+
+    def play(self):
+        self.state = "playing"
+        print(puzzle)
+        self.start()
+        while self.state == "playing":
+            time.sleep(.5)
+            self.update()
+
+        print(self.boards[0])
+
+        if self.state == "win":
+            print("win :D")
+        else:
+            print("lose :(")
                     
 
 
 
+mainBoard = [
+    Start1, Start1, Start1, Start1, Start1, Start1, Start1, 
+    Air, Air, Air, Air, Air, Air, Air,
+    Explosion, Air, Air, Air, Air, Hole, Air,
+    Air, Air, Air, Air, RightRamp, Air, Air,
+    Air, LeftRamp, Air, Air, Air, Air, Air,
+    Air, Air, Air, LeftRamp, Air, Air, Bomb,
+    Air, Air, LeftRamp, CircusExit, Air, Air, Air,
+    Air, Air, Air, Air, Air, Air, Air,
+    RightRamp, Air, Air, LeftRamp, Air, FigglesEntrance, Air,
+    Air, Air, Air, Air, Air, Air, RightRamp,
+    Air, Air, RightRamp, Air, LeftRamp, Air, Air,
+    Air, Helicopter, Air, Air, Air, Air, Air, 
+    Hole, Hole, Hole, Grandma, Hole, Hole, Hole
+]
 
-b = [Air for i in range(30)]
+figglesBoard = [
+    Air, FigglesExit, Air,
+    Air, Air, Air,
+    Air, Dice, Air,
+    Air, Air, Air, 
+    Air, Air, Air, 
+    Air, Air, Air, 
+    Hole, Hole, CircusEntrance
+]
 
-bongo = Board(5, 6, b)
 
-print(bongo)
+mainBoard = Board(7, 13, mainBoard)
+figglesBoard = Board(3, 7, figglesBoard)
+puzzle = Puzzle([mainBoard, figglesBoard], startTiles = [Start1])
+
+puzzle.play()
 
--- tiles.py
+++ tiles.py
@@ -1,6 +1,9 @@
+from random import random
+
 class Tile:
     def __init__(self, puzzle):
         self.puzzle = puzzle
+        self.disableAOE = False
 
 
     def __eq__(self, other):
@@ -9,17 +12,32 @@ class Tile:
 
     def balloon(self):
         return self.board.balloon
+    
+
+    def next(self, fart):
+        return fart.down()
+    
+
+    def activate(self, fart):
+        pass
 
 
-class Start(Tile):
-    shorthand = "air"
+    def aoe(self, fart):
+        return None
+    
+
+class Start1(Tile):
+    shorthand = "start1"
     emojiname = ":one:"
-    emoji = "1️⃣"
+    emoji = "1️⃣ "
     description = "start here"
 
 
-    def activate(self):
-        self.board.fart.fall()
+class Start2(Tile):
+    shorthand = "start2"
+    emojiname = ":two:"
+    emoji = "2️⃣ "
+    description = "start here"
 
 
 class Air(Tile):
@@ -29,22 +47,230 @@ class Air(Tile):
     description = "A blank tile. Fart passes through normally with no strings attached."
 
 
-    def activate(self):
-        self.puzzle.fart.fall()
+class RightRamp(Tile):
+    shorthand = "rightramp"
+    emojiname = ":black_large_square:"
+    emoji = "↘️ "
+    description = "push the the right lol"
+
+
+    def next(self, fart):
+        if fart.board.gravity[0] == 0:
+            left = fart.left()
+            down = fart.down()
+            return [left[0], down[1], fart.z]
+        elif fart.board.gravity[0] in [-1, 1]:
+            right = fart.right()
+            down = fart.down()
+            return [down[0], right[1], fart.z]
+        else:
+            return ValueError("invalid gravity (how?)")
 
 
 class LeftRamp(Tile):
-    shorthand = "air"
+    shorthand = "leftramp"
     emojiname = ":black_large_square:"
-    emoji = "⬛"
+    emoji = "🖋️ "
     description = "push the the left lol"
 
 
-    def activate(self):
-        self.puzzle.fart.fall()
-        self.puzzle.fart.left()
+    def next(self, fart):
+        if fart.board.gravity[0] == 0:
+            right = fart.right()
+            down = fart.down()
+            return [right[0], down[1], fart.z]
+        elif fart.board.gravity[0] in [-1, 1]:
+            left = fart.left()
+            down = fart.down()
+            return [down[0], left[1], fart.z]
+        else:
+            return ValueError("invalid gravity (how?)")
+        
+
+class GravityDown(Tile):
+    shorthand = "gravitydown"
+    emojiname = ":black_large_square:"
+    emoji = "⬇️ "
+    description = "switches board gravity to down"
 
 
-print(Air('h').shorthand)
+    def activate(self, fart):
+        board = fart.z
+        self.puzzle[board].setGravity("down")
+
+
+class GravityLeft(Tile):
+    shorthand = "gravityleft"
+    emojiname = ":black_large_square:"
+    emoji = "⬅️ "
+    description = "switches board gravity to left"
+
+
+    def activate(self, fart):
+        board = fart.z
+        self.puzzle[board].setGravity("left")
+
+
+class GravityRight(Tile):
+    shorthand = "gravityright"
+    emojiname = ":black_large_square:"
+    emoji = "➡️ "
+    description = "switches board gravity to right"
+
+
+    def activate(self, fart):
+        board = fart.z
+        self.puzzle[board].setGravity("right")
+
+
+class GravityUp(Tile):
+    shorthand = "gravityup"
+    emojiname = ":black_large_square:"
+    emoji = "⬆️ "
+    description = "switches board gravity to up"
+
+
+    def activate(self, fart):
+        board = fart.z
+        self.puzzle[board].setGravity("up")
+
+
+class Bomb(Tile):
+    shorthand = "bomb"
+    emojiname = ":bomb:"
+    emoji = "💣"
+    description = "teleport to explosion"
+
+
+    def next(self, fart):
+        return fart.coords()
+
+
+    def activate(self, fart):
+        puzzle = fart.puzzle
+        location = puzzle.findTileIndeces(Explosion)[0]
+        fart.moveto(location)
+        
+
+class Explosion(Tile):
+    shorthand = "boom"
+    emojiname = ":boom:"
+    emoji = "💥"
+    description = "bomb explodes here"
+
+
+class Helicopter(Tile):
+    shorthand = "helicopter"
+    emojiname = ":helicopter:"
+    emoji = "🚁"
+    description = "go back up"
+
+
+    def next(self, fart):
+        return [fart.x, 0, fart.z]
+    
+
+    def activate(self, fart):
+        fart.puzzle[fart] = Air(fart.puzzle)
+        
+
+class FigglesEntrance(Tile):
+    shorthand = "figglesentrance"
+    emojiname = ":clown:"
+    emoji = "🤡"
+    description = "go to figgles land"
+
+
+    def next(self, fart):
+        return fart.coords()
+
+
+    def activate(self, fart):
+        puzzle = fart.puzzle
+        location = puzzle.findTileIndeces(FigglesExit)[0]
+        fart.moveto(location)
+
+
+class FigglesExit(Tile):
+    shorthand = "figglesexit"
+    emojiname = ":clown:"
+    emoji = "🤡"
+    description = "you are now in figgles land"
+
+
+class CircusEntrance(Tile):
+    shorthand = "circusentrance"
+    emojiname = ":circus_tent:"
+    emoji = "🎪"
+    description = "leave figgles land"
+
+
+    def next(self, fart):
+        return fart.coords()
+
+
+    def activate(self, fart):
+        puzzle = fart.puzzle
+        location = puzzle.findTileIndeces(CircusExit)[0]
+        fart.moveto(location)
+
+
+class CircusExit(Tile):
+    shorthand = "circusexit"
+    emojiname = ":circus_tent:"
+    emoji = "🎪"
+    description = "you have left figgles land"
+
+
+class Hole(Tile):
+    shorthand = "hole"
+    emojiname = ":hole:"
+    emoji = "🕳️ "
+    description = "die"
+
+
+    def next(self, fart):
+        return fart.coords()
+    
+
+    def activate(self, fart):
+        fart.puzzle.state = "lose"
+
+
+class Grandma(Tile):
+    shorthand = "hole"
+    emojiname = ":older_woman:"
+    emoji = "👵"
+    description = "win"
+
+
+    def next(self, fart):
+        return fart.coords()
+    
+
+    def activate(self, fart):
+        fart.puzzle.state = "win"
+
+
+class Dice(Tile):
+    shorthand = "hole"
+    emojiname = ":older_woman:"
+    emoji = "🎲"
+    description = "win"
+
+
+    def next(self, fart):
+        choices = [fart.left(), fart.right()]
+        return random.choice(choices)
+
+
+
+
+
 
 # right ✒️ 💨 left🖋️
+
+
+# tile which you choose what direction ramp it is in the beginning
+# multiple boards, multiple fart starting locations
+# local gravity switch, global gravity switch
